cf9695f1012c46e2ca89f5717a4d7d285d7246b1
Michel Michaux <michel.michaux@student.pxl.be>
2026-05-11 15:41:32 +0200

    added a legit config

--- task36/run.py
+++ task36/run.py
@@ -4,7 +4,7 @@ import sys
 
 # ---------- CONFIGUREER HIER JE PARAMETERS ----------
 
-GITHUB_RAW_URL = "https://raw.githubusercontent.com/MichelMichauxPXL/EntNetDavyMichel_lab8.1/d440adf7d40bea67f1594e939e3e13dee85bf847/task36/router01.xml"
+GITHUB_RAW_URL = "https://raw.githubusercontent.com/MichelMichauxPXL/EntNetDavyMichel_lab8.1/refs/heads/main/task36/router01.xml"
 
 DEVICE = {
     "host": "172.17.1.1",          # IP van je IOS-XE toestel
